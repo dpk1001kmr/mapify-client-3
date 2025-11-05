347f59fcc37be01c2b81ccdfb44377c58b6dc852
dpk1001kmr <deepak8364kumar@gmail.com>
2025-11-05 18:08:04 +0530

    .

--- src/authConfig.js
+++ src/authConfig.js
@@ -3,8 +3,7 @@ export const msalConfig = {
     clientId: "b30fe25f-d839-4db1-ba00-bec20b82465d", // 👈 from Azure Portal
     authority:
       "https://login.microsoftonline.com/d6888770-182a-402b-81ae-b695d2c06904", // use your tenant ID if needed
-    redirectUri:
-      "https://mapify-client-3-294pjlhg8-deepak-kumars-projects-3b4205b0.vercel.app/auth/login", // 👈 must match in Azure app registration
+    redirectUri: "https://mapify-client-3.vercel.app/auth/login", // 👈 must match in Azure app registration
     // postLogoutRedirectUri: "http://localhost:5173",
   },
   cache: {
